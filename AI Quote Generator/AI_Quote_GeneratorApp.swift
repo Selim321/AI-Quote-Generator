69f8b8f8a875bcd979075d5b80a5ae3473b600ee
Selim Sahrane <s.sahrane321@gmail.com>
2025-05-05 16:12:58 +0100

    Initial Commit

---
+++ AI Quote Generator/AI_Quote_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Quote_GeneratorApp.swift
+//  AI Quote Generator
+//
+//  Created by Selim Sahrane on 05/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct AI_Quote_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI Quote GeneratorTests/AI_Quote_GeneratorTests.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Quote_GeneratorTests.swift
+//  AI Quote GeneratorTests
+//
+//  Created by Selim Sahrane on 05/05/2025.
+//
+
+import Testing
+@testable import AI_Quote_Generator
+
+struct AI_Quote_GeneratorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
